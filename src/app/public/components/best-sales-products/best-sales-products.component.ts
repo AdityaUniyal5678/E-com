9aa439db1aeef5ffd8fc87adaa2d3bd2f064668d
AdityaUniyal5678 <adityauniyal5678@gmail.com>
2024-11-29 17:05:15 +0530

    Created public and private folder structure partial inprogress.

---
+++ src/app/public/components/best-sales-products/best-sales-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'e-com-best-sales-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './best-sales-products.component.html',
+  styleUrl: './best-sales-products.component.scss'
+})
+export class BestSalesProductsComponent {
+
+}
---
+++ src/app/public/components/featured-products/featured-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'e-com-featured-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './featured-products.component.html',
+  styleUrl: './featured-products.component.scss'
+})
+export class FeaturedProductsComponent {
+
+}
